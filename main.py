6ad5a150d6bb1e408a46dbd502dc7e74fa1f987f
cmmasaba <mmasabacollins9@gmail.com>
2024-05-21 13:44:19 +0300

    Removed unused code

--- main.py
+++ main.py
@@ -197,11 +197,6 @@ async def bookRoom(request: Request):
         errors = "Invalid start and end time selected"
         return templates.TemplateResponse('book-room.html', {"request": request, "user_token": user_token, "errors": errors, "user_info": user, "rooms_list": rooms_list})
     
-    if datetime.date.fromisoformat(form['bookingDate']) < datetime.date.today():
-        rooms_list = [room.get("name") for room in rooms]
-        errors = "Select a present or future date"
-        return templates.TemplateResponse('book-room.html', {"request": request, "user_token": user_token, "errors": errors, "user_info": user, "rooms_list": rooms_list})  
-    
     if datetime.date.fromisoformat(form['bookingDate']) == datetime.date.today():
         '''If booking date is today and booking time is past'''
         if datetime.time.fromisoformat(form['bookingStartTime']) < datetime.time.fromisoformat(datetime.datetime.now().time().isoformat(timespec='minutes')):
@@ -448,11 +443,6 @@ async def editBooking(request: Request):
         rooms_list = [room.get("name") for room in rooms]
         errors = "Invalid start and end time selected"
         return templates.TemplateResponse('book-room.html', {"request": request, "user_token": user_token, "errors": errors, "user_info": user, "rooms_list": rooms_list})
-
-    if datetime.date.fromisoformat(form['bookingDate']) < datetime.date.today():
-        rooms_list = [room.get("name") for room in rooms]
-        errors = "Select a present or future date"
-        return templates.TemplateResponse('book-room.html', {"request": request, "user_token": user_token, "errors": errors, "user_info": user, "rooms_list": rooms_list})  
 
     if datetime.date.fromisoformat(form['bookingDate']) == datetime.date.today():
         '''If booking date is today and booking time is past'''
