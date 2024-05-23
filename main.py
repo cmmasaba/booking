54760d88077ed66888dfa4e9e08f6c2f332c973e
cmmasaba <mmasabacollins9@gmail.com>
2024-05-23 22:56:58 +0300

    Cleaning codebase

--- main.py
+++ main.py
@@ -358,9 +358,9 @@ async def bookRoom(request: Request):
                 errors = f"The room is already booked in this time slot: {booking['name']}, {booking['date']}, {booking['room']}, from {booking['from']} to {booking['to']}"
                 context = dict(
                     request=request,
-                    user_token=None,
+                    user_token=user_token,
                     errors=errors,
-                    user_info=None,
+                    user_info=user,
                     rooms=rooms_list
                 )
                 return templates.TemplateResponse('book-room.html', context=context)
